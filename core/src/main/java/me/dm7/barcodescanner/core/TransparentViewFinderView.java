1f89d5103f2427fb92ae450da0be0e717867c664
Jack Kwong <kwong999999@gmail.com>
2018-01-30 12:04:49 +0800

    fixed build error

--- core/src/main/java/me/dm7/barcodescanner/core/TransparentViewFinderView.java
+++ core/src/main/java/me/dm7/barcodescanner/core/TransparentViewFinderView.java
@@ -23,6 +23,61 @@ public class TransparentViewFinderView extends View implements IViewFinder {
     }
 
 
+    @Override
+    public void setLaserColor(int laserColor) {
+
+    }
+
+    @Override
+    public void setMaskColor(int maskColor) {
+
+    }
+
+    @Override
+    public void setBorderColor(int borderColor) {
+
+    }
+
+    @Override
+    public void setBorderStrokeWidth(int borderStrokeWidth) {
+
+    }
+
+    @Override
+    public void setBorderLineLength(int borderLineLength) {
+
+    }
+
+    @Override
+    public void setLaserEnabled(boolean isLaserEnabled) {
+
+    }
+
+    @Override
+    public void setBorderCornerRounded(boolean isBorderCornersRounded) {
+
+    }
+
+    @Override
+    public void setBorderAlpha(float alpha) {
+
+    }
+
+    @Override
+    public void setBorderCornerRadius(int borderCornersRadius) {
+
+    }
+
+    @Override
+    public void setViewFinderOffset(int offset) {
+
+    }
+
+    @Override
+    public void setSquareViewFinder(boolean isSquareViewFinder) {
+
+    }
+
     public void setupViewFinder() {
         updateFramingRect();
         invalidate();
